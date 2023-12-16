b36ec965d55329e9f76b372b62d3ae7a19ec90ec
deadshot002 <95534508+deadshot002@users.noreply.github.com>
2023-12-16 19:53:55 +0530

    commit_4

--- ewaste.js
+++ ewaste.js
@@ -4,31 +4,31 @@ document.querySelector('.sidebar').classList.toggle('not');
 document.querySelector('.haml').addEventListener("click", () => {
     document.querySelector('.sidebar').classList.toggle('sow');
     document.querySelector('.body').classList.toggle('stop');
-})
+});
 document.querySelector('.close').addEventListener("click", () => {
     document.querySelector('.sidebar').classList.toggle('sow');
     document.querySelector('.body').classList.toggle('stop');
-})
+});
 document.querySelector('.haml2').addEventListener("click", () => {
     document.querySelector('.sidebar2').classList.toggle('sow');
     document.querySelector('.body').classList.toggle('stop');
-})
+});
 document.querySelector('.close2').addEventListener("click", () => {
     document.querySelector('.sidebar2').classList.toggle('sow');
     document.querySelector('.body').classList.toggle('stop');
-})
+});
 document.querySelector('.close3').addEventListener("click", () => {
     document.querySelector('.sidebar').classList.toggle('sow');
-})
+});
 document.querySelector('.haml3').addEventListener("click", () => {
     document.querySelector('.sidebar2').classList.toggle('sow');
-})
+});
 document.querySelector('.close4').addEventListener("click", () => {
     document.querySelector('.sidebar2').classList.toggle('sow');
-})
+});
 document.querySelector('.haml4').addEventListener("click", () => {
     document.querySelector('.sidebar').classList.toggle('sow');
-})
+});
 
 $("a").click(function () {
     var pageId = $(this).attr("data-page");
