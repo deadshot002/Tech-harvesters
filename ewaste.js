909d04c1dff38358d2281861527a1073ed035320
deadshot002 <95534508+deadshot002@users.noreply.github.com>
2023-12-16 19:46:24 +0530

    commit_4

--- ewaste.js
+++ ewaste.js
@@ -4,7 +4,6 @@ document.querySelector('.sidebar').classList.toggle('not');
 document.querySelector('.haml').addEventListener("click", () => {
     document.querySelector('.sidebar').classList.toggle('sow');
     document.querySelector('.body').classList.toggle('stop');
-
 })
 document.querySelector('.close').addEventListener("click", () => {
     document.querySelector('.sidebar').classList.toggle('sow');
